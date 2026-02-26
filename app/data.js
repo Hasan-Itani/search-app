2e432a952a95ac47cdb4e8880b250ab0c103b53f
Hasan-Itani <itanihasan64@gmail.com>
2026-02-26 19:33:31 +0200

    commit

---
+++ app/data.js
@@ -0,0 +1,50 @@
+export const articles = [
+  {
+    title: "Understanding the difference between grid-template and grid-auto",
+    date: "2018-10-09",
+    description:
+      "With all the new properties related to CSS Grid Layout, one of th edistinctions that always confused me was the difference between the grid-template-* and grid-auto-* properties. Specifically the difference between grid-template-rows/columns and grid-outo-rows/columms. Although I knew ther were to d...",
+  },
+  {
+    title: "How to use the CSS grid-template-areas property",
+    date: "2018-10-08",
+    description:
+      "The grid-template-areas property is one of the most powerful features of CSS Grid Layout. It allows you to define a grid template by naming the areas of the grid. This can make your CSS much more readable and easier to maintain. In this article, we will explore how to use the grid-template-areas property and see some examples of how it can be used in practice.",
+  },
+  {
+    title: "How to create a responsive grid layout with CSS Grid",
+    date: "2018-10-07",
+    description:
+      "CSS Grid Layout is a powerful tool for creating responsive grid layouts. In this article, we will explore how to create a responsive grid layout using CSS Grid. We will look at how to define the grid structure, how to make it responsive, and how to use media queries to adjust the layout for different screen sizes.",
+  },
+  {
+    title: "How to use the CSS grid-auto-flow property",
+    date: "2018-10-06",
+    description:
+      "The grid-auto-flow property is a powerful feature of CSS Grid Layout that allows you to control how the grid items are placed in the grid. In this article, we will explore how to use the grid-auto-flow property and see some examples of how it can be used in practice.",
+  },
+  {
+    title: "How to create a grid layout with CSS Grid",
+    date: "2018-10-05",
+    description:
+      "CSS Grid Layout is a powerful tool for creating grid layouts on the web. In this article, we will explore how to create a grid layout using CSS Grid. We will look at how to define the grid structure, how to place items in the grid, and how to use various CSS Grid properties to create complex and responsive grid layouts.",
+  },
+  {
+    title: "How to use the CSS grid-template-columns property",
+    date: "2018-10-04",
+    description:
+      "The grid-template-columns property is a fundamental part of CSS Grid Layout that allows you to define the columns of your grid. In this article, we will explore how to use the grid-template-columns property and see some examples of how it can be used in practice.",
+  },
+  {
+    title: "How to use the CSS grid-template-rows property",
+    date: "2018-10-03",
+    description:
+      "The grid-template-rows property is a fundamental part of CSS Grid Layout that allows you to define the rows of your grid. In this article, we will explore how to use the grid-template-rows property and see some examples of how it can be used in practice.",
+  },
+  {
+    title: "How to use the CSS grid-gap property",
+    date: "2018-10-02",
+    description:
+      "The grid-gap property is a powerful feature of CSS Grid Layout that allows you to control the spacing between grid items. In this article, we will explore how to use the grid-gap property and see some examples of how it can be used in practice.",
+  },
+];
